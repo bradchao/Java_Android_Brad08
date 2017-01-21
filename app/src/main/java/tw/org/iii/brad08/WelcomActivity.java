5dfd202d94654f953e081b21d0f9962afbbfe864
brad <brad@brad.tw>
2017-01-21 11:43:44 +0800

    v9

--- app/src/main/java/tw/org/iii/brad08/WelcomActivity.java
+++ app/src/main/java/tw/org/iii/brad08/WelcomActivity.java
@@ -1,13 +1,31 @@
 package tw.org.iii.brad08;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomActivity extends AppCompatActivity {
+    private View welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcom);
+
+        welcome = findViewById(R.id.welcome);
+        welcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoMain();
+            }
+        });
+
     }
+
+    private void gotoMain(){
+        Intent it = new Intent(this, MainActivity.class);
+        startActivity(it);
+    }
+
 }
